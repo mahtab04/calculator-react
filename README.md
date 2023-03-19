e88a0bc0abd6f3178a97bd0b7137e966849455e1
Mahtab Alam <smmalam9@gmail.com>
2023-03-19 13:58:11 +0000

    comp[leted app

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# calculator-react
-Created with CodeSandbox
+# Calculator React Project
+
+This is a simple calculator application built with React. It allows users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division.
+
+# Installation
+
+## To install this project, follow these steps:
+
+1. Clone the repository to your local machine
+  - `git clone https://github.com/mahtab04/calculator-react.git`
+2. Navigate to the project directory
+    - `cd calculator-react`
+3. Install the dependencies
+    - `npm install`
+4.  Start the development server:
+    - `npm start`
+5.  Open the application in your browser:
+    - `http://localhost:3000`
+
+# Usage
+
+To use the calculator, simply click the buttons on the screen to enter numbers and perform operations. The calculator supports the following operations:
+
+- Addition (+)
+- Subtraction (-)
+- Multiplication (*)
+- Division (/)
+
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,10 +1,82 @@
-import "./styles.css";
+import React, { useState } from 'react'
+import './Styles.css';
+import Button from './Button/Button';
 
-export default function App() {
+
+function App() {
+
+  const [value, setValue] = useState('');
+
+  // Check if the value is integer
+  const isInt = (n) => {
+    return Number(n) === n && n % 1 === 0;
+  }
+
+  // Check if the value is decimal
+  const isFloat = (n) => {
+    return Number(n) === n && n % 1 !== 0;
+  }
+
+  // gathers all the input digits
+  const handleDigits = (val) => {
+    setValue(value + val)
+  }
+
+  const evaluateDigits = () => {
+    try {
+      // evaluates the input digits
+      let answer = eval(value)
+      if (isFloat(answer)) {
+        setValue(answer.toFixed(2))
+      } else if (isInt(answer)) {
+        setValue(answer)
+      }
+    } catch (error) {
+      // Alert the user when the user input format is wrong
+      // Prevents the app from crashing
+      alert(error.message);
+    }
+  }
+
+  // Clear all the values input
+  const clearDigits = () => {
+    setValue("")
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="evaluated-result">
+        <h2 className="result-height">{value}</h2>
+      </div>
+      <div className="digits">
+        <Button valueClick={handleDigits}>7</Button>
+        <Button valueClick={handleDigits}>8</Button>
+        <Button valueClick={handleDigits}>9</Button>
+        <Button valueClick={handleDigits}>/</Button>
+      </div>
+      <div className="digits">
+        <Button valueClick={handleDigits}>4</Button>
+        <Button valueClick={handleDigits}>5</Button>
+        <Button valueClick={handleDigits}>6</Button>
+        <Button valueClick={handleDigits}>*</Button>
+      </div>
+      <div className="digits">
+        <Button valueClick={handleDigits}>1</Button>
+        <Button valueClick={handleDigits}>2</Button>
+        <Button valueClick={handleDigits}>3</Button>
+        <Button valueClick={handleDigits}>+</Button>
+      </div>
+      <div className="digits">
+        <Button valueClick={handleDigits}>.</Button>
+        <Button valueClick={handleDigits}>0</Button>
+        <Button valueClick={evaluateDigits}>=</Button>
+        <Button valueClick={handleDigits}>-</Button>
+      </div>
+      <div className="digits">
+        <button className="clear" onClick={clearDigits}>CLEAR</button>
+      </div>
     </div>
   );
 }
+
+export default App;
